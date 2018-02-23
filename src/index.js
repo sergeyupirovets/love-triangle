f560817f55c6035dd8b12ed094677b783831beba
sergeyupirovets <sergey.upirovets@gmail.com>
2018-02-23 14:53:53 +0300

    love_triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let triangles = 0;
+    preferences.forEach((item, i, preferences ) => {
+        if (item === i + 1) return;
+
+        const second = preferences[item - 1];
+        const third = preferences[second - 1];
+
+        if (third === i + 1) {
+            triangles++;
+        }
+
+    });
+    return triangles / 3;
 };
